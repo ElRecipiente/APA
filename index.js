21dc4d6d78186f28122b6513c46c8bf08e95d502
ElRecipiente <nicolas.droux.2b@gmail.com>
2022-12-20 14:25:40 +0100

    add arrow with smooth scroll

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+// LA BANNIERE
+const bannerScroll = document.querySelector(".arrow-bottom");
+let scroll = this.scrollY;
+let magicBanner = document.getElementById('magic-banner')
+let headerHeight = document.getElementById("hvideo").getBoundingClientRect().height
+
+// window.addEventListener("scroll", () => {
+//     const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+//     console.log(scrollTop, clientHeight, scrollHeight);
+// })
+
+bannerScroll.addEventListener("click", () => {
+    if(scrollY < headerHeight) {
+        window.scrollTo(0,headerHeight-200)
+    };
+});
+
 window.addEventListener("scroll", (event) => {
-    let scroll = this.scrollY;
-    let magicBanner = document.getElementById('magic-banner')
-    let headerHeight = document.getElementById("hvideo").getBoundingClientRect().height
-    if (scrollY > headerHeight) {
+    if (scrollY >= headerHeight-200) {
         magicBanner.style.opacity = 1;
     } else {
         magicBanner.style.opacity = 0;
     }
 });
+
